bff85e7b08fc6a43152474985c545e98758a9a16
Mikhail Kazlou1 <kazlou.ms@gmail.com>
2024-11-16 21:52:48 +0200

    rename to yml

--- src/api-docs/openapi-writer.ts
+++ src/api-docs/openapi-writer.ts
@@ -4,13 +4,13 @@ import { stringify } from 'yaml'
 import { openApiDoc } from './openapi-generator'
 // Generate OpenAPI docs with Zod
 
-// convert OpenAPI document to YAML
-const yamlDoc = stringify(openApiDoc)
+// convert OpenAPI document to YML
+const ymlDoc = stringify(openApiDoc)
 const scriptDir = path.resolve(__dirname)
 
-// write YAML file
-fs.writeFileSync(`${scriptDir}/openapi.yaml`, yamlDoc)
-console.log('OpenAPI document generated in YAML format.')
+// write YML file
+fs.writeFileSync(`${scriptDir}/openapi.yml`, ymlDoc)
+console.log('OpenAPI document generated in YML format.')
 
 // JSON version
 const jsonDoc = JSON.stringify(openApiDoc, null, 2)
